8623727fd5460542f473b7934be1feb8ea6fcf24
ndong220 <dong26k@gmail.com>
2022-02-22 13:00:53 +0700

    first commit

---
+++ Watch/resources/views/frontend/contact.blade.php
@@ -0,0 +1,36 @@
+@extends('frontend.master')
+@section('title')
+	<title>Liên hệ</title>
+@endsection
+@section('content')
+<div class="container">
+	<h3 style="text-align: center;">Liên hệ</h3>
+	<hr>
+	<div class="row">
+		<div class="col-sm-5">
+			<div class="row mb-1">
+				<h6 class="col-sm-4"><i class="fas fa-phone-alt"></i> Điện thoại</h6><div class="col-sm-8"> 0385.044.800</div>
+			</div>
+			<div class="row mb-1">
+				<h6 class="col-sm-4"><i class="fas fa-envelope"></i> Email</h6><div class="col-sm-8"> Dong26K@gmail.com</div>
+			</div>
+			<hr>
+			<div class="card mb-3">
+				<h5 class="card-header"><i class="fab fa-facebook" style="color: Dodgerblue"></i> Facebook</h5>
+				<div class="card-body">
+					<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdonghovandinh&tabs=timeline&width=350&height=350&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="350" height="350" style="border:none;overflow:hidden; width: 100%" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+				</div>
+			</div>
+			
+		</div>
+		<div class="col-sm-7">
+			<div class="card">
+			<h5 class="card-header"><i class="fas fa-map-marked-alt" style="color: red"></i> Bản đồ</h5>
+				<div class="card-body">
+	   	 		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3726.3993680403632!2d106.31045901440585!3d20.936471196299628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31359b125ee04d43%3A0xc7e448f3048eff7d!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBTxrAgUGjhuqFtIEvhu7kgVGh14bqtdCBIxrBuZyBZw6pu!5e0!3m2!1svi!2s!4v1621929484306!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+	   	 		</div>
+   	 		</div>
+    	</div>
+    </div>
+</div>
+@endsection
